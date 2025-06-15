e2ed1ddeed1f631d03262e5232126061f2aecebb
Hanjocl <hanjo.cl@gmail.com>
2025-06-15 15:59:04 +0200

    Add libs

---
+++ DeckRC/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <concepts>
+#include "include\operations.h"
+#include "include\dog.h"
+
+int main() {
+    Dog dog1("Buddy");
+    dog1.print_info();
+
+    return 0;
+} 
