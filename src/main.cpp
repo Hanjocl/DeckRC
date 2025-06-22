e859084114b1306b8caaa8649ad69579b265a40e
Hanjocl <hanjo.cl@gmail.com>
2025-06-22 20:12:42 +0200

    Integrated Windows config with Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
-#include <QQmlApplicationEngine>
 #include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/DeckRC/src/QML/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule(CONFIG_NAME, "Main");
+    engine.load(url);
 
     return app.exec();
 }
